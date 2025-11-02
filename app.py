66acabd0095ace8c34cb2389dfd0158263cd6b79
alex.mwangi <alex.mwangi@transsion.com>
2025-11-02 13:16:09 +0300

    kl

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import sklearn
 # Load the model
 @st.cache_resource
 def load_model():
-    return joblib.load("best_employee_performance_model_sklearn172.joblib")
+    return joblib.load("best_employee_performance_model_sklearn.joblib")
 
 def main():
     st.title("Employee Performance Predictor")
